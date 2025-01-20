760a0ae5b0d0c1c9a60697b6726f669ae2e52713
Admin <sohilasobhysadek@gmail.com>
2025-01-20 14:51:24 +0200

    first commit

---
+++ Index.js
@@ -0,0 +1,7 @@
+document.getElementById('darkBtn').addEventListener('click', function () {
+    const body = document.getElementById('Body');
+    body.classList.toggle('bgLight');
+    body.classList.toggle('bgDark');
+    body.classList.toggle('dark')
+});
+
